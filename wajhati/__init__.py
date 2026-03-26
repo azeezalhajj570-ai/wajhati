4b17fd12becf64258d1420f9f78fe2bb6c933262
azeez <azeezalhajj570@gmail.com>
2026-03-26 23:54:27 +0000

    Bootstrap existing admin user on startup

--- wajhati/__init__.py
+++ wajhati/__init__.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import inspect, text
 
 from config import Config
+from wajhati.models import User
 from wajhati.translations import tr_category, tr_city, tr_description, tr_destination, tr_interest_tag, tr_season
 
 db = SQLAlchemy()
@@ -40,6 +43,7 @@ def create_app(config_class=Config):
 
         db.create_all()
         _ensure_user_profile_columns()
+        _ensure_admin_user()
         seed_demo_destinations()
 
     return app
@@ -61,4 +65,24 @@ def _ensure_user_profile_columns():
     for statement in statements:
         db.session.execute(text(statement))
     if statements:
+        db.session.commit()
+
+
+def _ensure_admin_user():
+    admin_email = os.environ.get("ADMIN_EMAIL", "").strip().lower()
+    updated = False
+
+    if admin_email:
+        admin_user = User.query.filter_by(email=admin_email).first()
+        if admin_user and not admin_user.is_admin:
+            admin_user.is_admin = True
+            updated = True
+
+    if User.query.count() and User.query.filter_by(is_admin=True).count() == 0:
+        first_user = User.query.order_by(User.id.asc()).first()
+        if first_user and not first_user.is_admin:
+            first_user.is_admin = True
+            updated = True
+
+    if updated:
         db.session.commit()
